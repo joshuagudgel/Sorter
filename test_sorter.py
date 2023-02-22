b539c54514a009ceea3dea16795dd9762a559644
Josh <joshuabgudgel@gmail.com>
2023-02-21 21:56:43 -0800

    Added setup method to simplify

--- test_sorter.py
+++ test_sorter.py
@@ -4,18 +4,22 @@ from sorter import Sorter
 UNSORTED: list = [4,2,1,6,9,3,7,5,8,0]
 EMPTY: list = []
 
-class TestTimsort(unittest.TestCase):
+class TestSorter(unittest.TestCase):
+
+    # setUp()
+    def setUp(self):
+        self.sorter = Sorter()
 
     # just an example    
     def test_sorter_timsort(self):
-        sorter = Sorter()
-        self.assertEqual(sorter.sorter_timsort(UNSORTED),sorted(UNSORTED))
+        self.assertEqual(self.sorter.sorter_timsort(UNSORTED),sorted(UNSORTED))
 
     """ define specifications for 'Sorter' """
+
+    """ timsort """
     # timsort returns an ordered list
     def test_timsort_in_order_unsorted(self):
-        sorter = Sorter()
-        result = sorter.sorter_timsort(UNSORTED)
+        result = self.sorter.sorter_timsort(UNSORTED)
         for i in range(0,len(result)):
             if i > 0:
                 curr = result[i]
@@ -24,20 +28,17 @@ class TestTimsort(unittest.TestCase):
     
     # timsort returns empty from empty input
     def test_timsort_empty(self):
-        sorter = Sorter()
-        self.assertEqual(sorter.sorter_timsort(EMPTY),sorted(EMPTY))
+        self.assertEqual(self.sorter.sorter_timsort(EMPTY),sorted(EMPTY))
 
     # object variable runtime is afloat
     def test_sorter_runtime_is_float(self):
-        sorter = Sorter()
-        self.assertIsInstance(sorter.get_runtime(), float)
+        self.assertIsInstance(self.sorter.get_runtime(), float)
     
     # runtime is updated after sort
     def test_sorter_runtime_updates(self):
-        sorter = Sorter()
-        initial_runtime = sorter.get_runtime()
-        sorter.sorter_timsort(UNSORTED)
-        self.assertNotEqual(sorter.get_runtime(), initial_runtime)
+        initial_runtime = self.sorter.get_runtime()
+        self.sorter.sorter_timsort(UNSORTED)
+        self.assertNotEqual(self.sorter.get_runtime(), initial_runtime)
 
 if __name__ == '__main__':
     unittest.main()
