a27a21fe6ee823d126199c85f41239e30c424a5e
Josh <joshuabgudgel@gmail.com>
2023-02-23 22:41:07 -0800

    shuffle sort method and helper to check order

--- sorter.py
+++ sorter.py
@@ -1,3 +1,4 @@
+import random
 class Sorter:
     def __init__(self):
         self._runtime = 0.0
@@ -7,3 +8,15 @@ class Sorter:
 
     def sorter_timsort(self, input_list):
         return sorted(input_list)
+    
+    def sorter_shuffle(self, input_list):
+        target = input_list.copy()
+        while not self.list_in_order(target):
+            random.shuffle(target)
+        return target
+
+    def list_in_order(self, input_list):
+        for i in range(0, len(input_list)):
+            if i > 0 and input_list[i] < input_list[i-1]:
+                return False
+        return True
