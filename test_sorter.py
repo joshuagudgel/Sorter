07ee3d5c3f8b8050f29bcad65d1dfdc9677b9d1e
Josh <joshuabgudgel@gmail.com>
2023-02-26 20:21:48 -0800

    subTest allows for multiple lists to be checked with the same test (confirming that result is in order)

--- test_sorter.py
+++ test_sorter.py
@@ -14,14 +14,16 @@ class TestSorter(unittest.TestCase):
 
     # Define specifications for 'Sorter'
     
-    # Timsort returns an ordered list
-    def test_timsort_in_order_unsorted(self):
-        result = self.sorter.sorter_timsort(my_constants.UNSORTED)
-        for i in range(0,len(result)):
-            if i > 0:
-                curr = result[i]
-                self.assertGreater(curr,prev)
-            prev = result[i]
+    # Timsort returns an ordered list for each item in test list
+    def test_timsort_in_order(self):
+        for i in range(0, len(my_constants.LISTS)):
+            with self.subTest(i=i):
+                result = self.sorter.sorter_timsort(my_constants.LISTS[i])
+                for j in range(0,len(result)):
+                    if j > 0:
+                        curr = result[j]
+                        self.assertGreaterEqual(curr,prev)
+                    prev = result[j]
     
     # Timsort returns empty from empty input
     def test_timsort_empty(self):
@@ -40,12 +42,12 @@ class TestSorter(unittest.TestCase):
         for i in range(0,len(result)):
             if i > 0:
                 curr = result[i]
-                self.assertGreater(curr,prev)
+                self.assertGreaterEqual(curr,prev)
             prev = result[i]
 
     # Shuffle sort returns empty from empty input
     def test_shufflesort_empty(self):
-        self.assertEqual(self.sorter.sorter_shuffle(my_constants.EMPTY),sorted(my_constants.EMPTY))
+        self.assertEqual(self.sorter.sorter_shuffle(my_constants.EMPTY),[])
     
     # Runtime is updated after shuffle sort
     # TODO: Has a chance of having a false negative
@@ -54,18 +56,20 @@ class TestSorter(unittest.TestCase):
         self.sorter.sorter_shuffle(my_constants.UNSORTED)
         self.assertNotEqual(self.sorter.get_runtime(), initial_runtime)
 
-    # Merge sort returns an ordered list
-    def test_merge_unsorted_short(self):
-        result = self.sorter.sorter_merge(my_constants.UNSORTED_SHORT)
-        for i in range(0,len(result)):
-            if i > 0:
-                curr = result[i]
-                self.assertGreater(curr,prev)
-            prev = result[i]
+    # Merge sort returns an ordered list for each list in tests list
+    def test_mergesort_in_order(self):
+        for i in range(0, len(my_constants.LISTS)):
+            with self.subTest(i=i):
+                result = self.sorter.sorter_merge(my_constants.LISTS[i])
+                for j in range(0,len(result)):
+                    if j > 0:
+                        curr = result[j]
+                        self.assertGreaterEqual(curr,prev)
+                    prev = result[j]
 
     # Merge sort returns empty from empty input
     def test_mergesort_empty(self):
-        self.assertEqual(self.sorter.sorter_merge(my_constants.EMPTY),sorted(my_constants.EMPTY))
+        self.assertEqual(self.sorter.sorter_merge(my_constants.EMPTY),[])
 
     # Runtime is updated after merge sort
     # TODO: Has a chance of having a false negative
