3decd32a4b0f4bed8566ef3845aba6935553e7b8
Josh <joshuabgudgel@gmail.com>
2023-03-21 10:03:55 -0700

    removed implementations and clutter for fresh start

--- sorter.py
+++ sorter.py
@@ -1,100 +1,44 @@
 import random
 import timeit
 class Sorter:
-    def __init__(self):
-        self._runtime = 0.0
-    
-    def get_runtime(self):
-        return self._runtime
 
+    # Python's built-in sort
+    # O(nlogn)
+    # Uses Timsort, a hybrid of merge sort and insertion sort
     def sorter_timsort(self, input_list):
+        sort_type = "Tim Sort"
         result = sorted(input_list)
         return result
     
     # Shuffle sort
     # Randomly rearrange the list using python's random.shuffle
     # until all elements are in order
+    # This is should be tested on short lists
     def sorter_shuffle(self, input_list):
-        target = input_list.copy()
-        while not self.list_in_order(target):
-            random.shuffle(target)
-        return target
+        sort_type = "Shuffle Sort"
+        return NotImplementedError
 
     # Bubble sort
     # O(n^2)
     # Cycle through list over and over
     # pushing the largest number to the end until list is in order
     def sorter_bubble(self, input_list):
-        result = input_list.copy()
-        swap_made = True
-        # Continue bubbling high values along the list as long as at least one swap is made
-        while swap_made:
-            swap_made = False
-            for i in range(0,len(result)):
-                # Swap value with adjacent value in list if left > right
-                if i < len(result)-1 and result[i] > result[i+1]:
-                    temp = result[i]
-                    result[i] = result[i+1]
-                    result[i+1] = temp
-                    swap_made = True
-        return result
+        sort_type = "Bubble Sort"
+        return NotImplementedError
 
     # Merge sort
     # O(nlogn) - the logn comes from the max height of binary tree
     # Divide and conquer
     def sorter_merge(self, input_list):
         sort_type = "Merge  Sort"
-        if len(input_list) <= 1:
-            return input_list
-        l, r = [], []
-        # Call mergesort on left half and right half
-        if len(input_list) > 1:
-            l = self.sorter_merge(input_list[:len(input_list)//2])
-            r = self.sorter_merge(input_list[len(input_list)//2:])
-        # Merge the two sides efficiently
-        result = self.merge(l,r)
-        return result
+        return NotImplementedError
     
     # Helpers
 
-    # Write output to log file
+    # Write sort type/input/output to log file
     def write_to_log(self, input_list, output_list, sort_type):
         with open("sorter.log", "a") as log:
             log.write("Sort type: " + sort_type + "\n")
             log.write("Input list: " + str(input_list) + "\n")
             log.write("Output list: " + str(output_list) + "\n")
             log.write("\n")
-
-    # Merge sort helper
-    def merge(self, left_half, right_half):
-        result = []
-        # While both lists have elements
-        while len(left_half) > 0 and len(right_half) > 0:
-            if(left_half[0] > right_half[0]):
-                result.append(right_half[0])
-                right_half = right_half[1:]
-            else:
-                result.append(left_half[0])
-                left_half = left_half[1:]
-        # While elements remain in left half
-        if len(left_half) > 0:
-            while len(left_half) > 0:
-                result.append(left_half[0])
-                left_half = left_half[1:]
-        # While elements remain in right half
-        elif len(right_half) > 0:
-            while len(right_half) > 0:
-                result.append(right_half[0])
-                right_half = right_half[1:]
-        return result
-
-    # Check if list is in order
-    def list_in_order(self, input_list):
-        for i in range(0, len(input_list)):
-            if i > 0 and input_list[i] < input_list[i-1]:
-                return False
-        return True
-    
-s = Sorter()
-input_list = [1, 5, 3, 2, 4]
-print(s.sorter_merge(input_list))
--- test_sorter.py
+++ test_sorter.py
@@ -6,56 +6,31 @@ class TestSorter():
 
     sorter = Sorter()
 
-    # Initial example comparing two methods doing the same thing    
-    @pytest.mark.parametrize("test_input,expected", my_constants.LISTS)
-    def test_sorter_timsort(self, test_input: tuple[list[int], list[int]], expected: tuple[list[int], list[int]]):
-        #self.sorter = Sorter()
-        assert self.sorter.sorter_timsort(test_input) == expected
-
-    # Define specifications for 'Sorter'
-    
-    # Timsort returns an ordered list for each item in test list
-    @pytest.mark.parametrize("test_input,expected", my_constants.LISTS)
-    def test_timsort_in_order(self, test_input: tuple[list[int], list[int]],expected: tuple[list[int], list[int]]):
-        #sorter = Sorter()
-        assert self.sorter.sorter_timsort(test_input) == expected
-    
-    # Timsort returns empty from empty input
-    def test_timsort_empty(self):
-        #sorter = Sorter()
-        assert self.sorter.sorter_timsort(my_constants.EMPTY) == []
-
     # Shuffle sort returns an ordered list
     # Use a short list to test because it would take too long normally
     def test_shuffle_unsorted_short(self):
-        sorter = Sorter()
-        assert sorter.sorter_shuffle(my_constants.UNSORTED_SHORT) == sorted(my_constants.UNSORTED_SHORT)
+        assert self.sorter.sorter_shuffle(my_constants.UNSORTED_SHORT) == sorted(my_constants.UNSORTED_SHORT)
 
     # Shuffle sort returns empty from empty input
     def test_shufflesort_empty(self):
-        sorter = Sorter()
-        assert sorter.sorter_shuffle(my_constants.EMPTY) == []
+        assert self.sorter.sorter_shuffle(my_constants.EMPTY) == []
 
     # Merge sort returns an ordered list for each list in tests list
     @pytest.mark.parametrize("test_input,expected", my_constants.LISTS)
     def test_mergesort_in_order(self, test_input: tuple[list[int], list[int]], expected: tuple[list[int], list[int]]):
-        #sorter = Sorter()
         assert self.sorter.sorter_merge(test_input) == expected
 
     # Merge sort returns empty from empty input
     def test_mergesort_empty(self):
-        #sorter = Sorter()
         assert self.sorter.sorter_merge(my_constants.EMPTY) == []
 
     # Bubble sort returns an ordered list for each list in tests list
     @pytest.mark.parametrize("test_input,expected", my_constants.LISTS)
     def test_bubblesort_in_order(self, test_input: tuple[list[int], list[int]], expected: tuple[list[int], list[int]]):
-        #sorter = Sorter()
         assert self.sorter.sorter_bubble(test_input) == expected
 
     # Bubble sort returns empty from empty input
     def test_bubblesort_empty(self):
-        #sorter = Sorter()
         assert self.sorter.sorter_bubble(my_constants.EMPTY) == []
 
     # TBD: future implementation
@@ -63,27 +38,23 @@ class TestSorter():
     # Runtime is updated after timsort
     @pytest.mark.parametrize("test_input,expected", my_constants.LISTS)
     def test_sorter_timsort_runtime_updates(self, test_input, expected):
-        #sorter = Sorter()
         initial_runtime = self.sorter.get_runtime()
         self.sorter.sorter_timsort(test_input)
         assert self.sorter.get_runtime() != initial_runtime
     
     # Runtime is updated after shuffle sort
     def test_sorter_shuffle_runtime_updates(self):
-        #sorter = Sorter()
         initial_runtime = self.sorter.get_runtime()
         self.sorter.sorter_shuffle(my_constants.UNSORTED)
         assert self.sorter.get_runtime() != initial_runtime
 
     # Runtime is updated after bubble sort
     def test_sorter_bubble_runtime_updates(self, test_input, expected):
-        #sorter = Sorter()
         initial_runtime = self.sorter.get_runtime()
         self.sorter.sorter_bubble(my_constants.UNSORTED)
         assert self.sorter.get_runtime() != initial_runtime
 
     # Runtime is type: afloat
     def test_sorter_runtime_is_float(self):
-        self.sorter = Sorter()
         self.assertIsInstance(self.sorter.get_runtime(), float)
     """
